a39010c04d4f8b2d27045760eec527b605acd06b
MatsDK <matsdekort257@gmail.com>
2022-03-26 13:56:56 +0000

    get/put record with tcp api

--- src/behaviour.rs
+++ src/behaviour.rs
@@ -25,13 +25,32 @@ pub struct Query {
 }
 
 #[derive(NetworkBehaviour)]
-#[behaviour(event_process = true)]
+#[behaviour(out_event = "OutEvent", event_process = false)]
 pub struct MyBehaviour {
 	pub kademlia: Kademlia<MemoryStore>,
 	pub mdns: Mdns,
 	#[behaviour(ignore)]
 	pub queries: Arc<Mutex<HashMap<QueryId, Query>>>
 }	
+
+#[derive(Debug)]
+pub enum OutEvent {
+    Kademlia(KademliaEvent),
+    Mdns(MdnsEvent)
+}
+
+impl From<KademliaEvent> for OutEvent {
+	fn from(event: KademliaEvent) -> Self {
+		println!("{:?}", event);
+		Self::Kademlia(event)
+	}
+}
+
+impl From<MdnsEvent> for OutEvent {
+	fn from(event: MdnsEvent) -> Self {
+		Self::Mdns(event)
+	}
+}
 
 impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
 	fn inject_event(&mut self, event: MdnsEvent) {
@@ -48,29 +67,29 @@ impl NetworkBehaviourEventProcess<KademliaEvent> for MyBehaviour {
 	fn inject_event(&mut self, msg: KademliaEvent) {
 		match msg {
 			KademliaEvent::OutboundQueryCompleted { result, id, .. } => match result {
-				QueryResult::GetRecord(Ok(ok)) => {
-					for PeerRecord {
-						record: Record { key, value, .. },
-						..
-					} in ok.records {
-						let query = self.queries.lock().unwrap().remove(&id).unwrap();
-						let entry: Entry = serde_json::from_str(&str::from_utf8(&value).unwrap()).unwrap();
+				QueryResult::GetRecord(Ok(_ok)) => {
+					// for PeerRecord {
+					// 	record: Record { key, value, .. },
+					// 	..
+					// } in ok.records {
+					// 	let query = self.queries.lock().unwrap().remove(&id).unwrap();
+					// 	let entry: Entry = serde_json::from_str(&str::from_utf8(&value).unwrap()).unwrap();
 
-						let parts: Vec<String> = query.location.split("/").map(|s| s.to_string()).collect();
+					// 	let parts: Vec<String> = query.location.split("/").map(|s| s.to_string()).collect();
 
-						if parts.len() == 1 {
-							if entry.user == query.username || entry.public || entry.read_users.contains(&query.username)  {
-								println!("{:?}", entry);
-							} else {
-								println!("Read access to {:?} not allowed", key);
-							}
-						} else {
-							let path = parts[1..].join("/");
-						}
-					}
+					// 	if parts.len() == 1 {
+					// 		if entry.user == query.username || entry.public || entry.read_users.contains(&query.username)  {
+					// 			println!("{:?}", entry);
+					// 		} else {
+					// 			println!("Read access to {:?} not allowed", key);
+					// 		}
+					// 	} else {
+					// 		let _path = parts[1..].join("/");
+					// 	}
+					// }
 				},
 				QueryResult::GetRecord(Err(err)) => {
-					self.queries.lock().unwrap().remove(&id).unwrap();
+					// self.queries.lock().unwrap().remove(&id).unwrap();
 					eprintln!("Failed to get record: {:?}", err);
 				},
 				_ => {
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use libp2p::kad::{
      Kademlia,
      record::Key,
      Quorum,
-     Record
+     Record,
+     KademliaEvent
 };
 use libp2p::{
     development_transport, 
@@ -11,7 +12,7 @@ use libp2p::{
     mdns::{Mdns, MdnsConfig},
     PeerId, 
     Swarm,
-    swarm::{ SwarmEvent}
+    swarm::SwarmEvent
 };
 use async_std::{task, io};
 use futures::{prelude::*};
@@ -20,15 +21,16 @@ use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
 // use rand::{thread_rng, Rng, distributions::Alphanumeric};
 use sha2::{Sha256, Digest};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpListener; 
 use tokio::io::AsyncReadExt;
+use serde::Deserialize;
 
 mod entry;
 mod behaviour;
-mod routes;
+// mod routes;
 
 use entry::{Entry, Children};
-use behaviour::{MyBehaviour, Query};
+use behaviour::{MyBehaviour, Query, OutEvent};
 
 async fn create_swarm() -> Swarm<MyBehaviour> {
 	let local_key = identity::Keypair::generate_ed25519();
@@ -47,6 +49,11 @@ async fn create_swarm() -> Swarm<MyBehaviour> {
         Swarm::new(transport, behaviour, local_peer_id)
 }
 
+#[derive(Debug, Deserialize)]
+struct GetRecordQuery {
+	location: String,
+	username: String
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -64,66 +71,88 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	// Listen on all interfaces and whatever port the OS assigns.
 	swarm.listen_on("/ip4/192.168.0.164/tcp/0".parse()?)?;
 
-	// let r = routes::create_routes(&mut swarm.behaviour_mut());
 	let listener = TcpListener::bind("192.168.0.164:8000").await?;
 	
 	loop {
 		tokio::select! {
-			// _server = warp::serve(r.clone()).run(([192, 168, 0, 164], 8000)) => {
-			// 	println!("test");
-			// },
 			s = listener.accept() => {
 				let (mut socket, _) = s.unwrap();
 
 				let mut buffer = [0; 1024];
-				let bytes_read = socket.read(&mut buffer).await.unwrap();
+				let bytes_read = match socket.read(&mut buffer).await {
+					Ok(b) => b,
+					Err(_error) => continue,
+				};
 
-				if buffer.starts_with(b"POST /put HTTP/1.1\r\n") {
-					let mut headers = [httparse::EMPTY_HEADER; 64];
-					let mut _req = httparse::Request::new(&mut headers);
+				let mut headers = [httparse::EMPTY_HEADER; 64];
+				let mut req = httparse::Request::new(&mut headers);
+				match req.parse(&buffer) {
+					Ok(..) => {},
+					Err(error) => {
+						eprintln!("{:?}", error);
+						continue
+					}
+				};
 
-					_req.parse(&buffer).unwrap();
+				let request = String::from_utf8_lossy(&buffer[..bytes_read]);
+				let mut parts = request.split("\r\n\r\n");
+				match parts.next() {
+					Some(_val) => {},
+					None => {
+						eprintln!("Expected body");
+						continue
+					}
+				};
 
-					let request = String::from_utf8_lossy(&buffer[..bytes_read]);
-					let mut parts = request.split("\r\n\r\n");
-					parts.next().unwrap();
+				let body = match parts.next() {
+					Some(val) => val.to_string(),
+					None => "".to_string()
+				};
 
-					let body = {
-						match parts.next() {
-							Some(val) => val.to_string(),
-							None => "".to_string()
+				if body.len() == 0 {
+					continue
+				}
+
+				println!("{}, {}", req.method.unwrap(), req.path.unwrap());
+
+				let behaviour = swarm.behaviour_mut();
+
+				if req.method.unwrap() == "POST" && req.path.unwrap() == "/put" {
+					let entry: Entry = serde_json::from_str(&body).unwrap();
+
+					let value = serde_json::to_vec(&entry).unwrap();
+
+					let mut hasher = Sha256::new();
+					hasher.update(format!("{}{}", entry.user, entry.name));
+					let key: String = format!("e_{:X}", hasher.finalize());
+
+					let record = Record {
+						key: Key::new(&key),
+						value,
+						publisher: None,
+						expires: None,
+					};
+					println!("{:?}", entry);
+
+
+					behaviour.kademlia
+						.put_record(record, Quorum::One)
+						.expect("Failed to store record locally.");
+				} else if req.method.unwrap() == "GET" && req.path.unwrap() == "/get" {
+					let query: GetRecordQuery = serde_json::from_str(&body).unwrap();
+					let key = get_location_key(query.location.clone());
+					println!("{:?}\n{:?}", key, query);
+
+					let query_id = behaviour.kademlia.get_record(&key, Quorum::One);
+
+					let res = loop {
+						if let SwarmEvent::Behaviour(OutEvent::Kademlia(KademliaEvent::OutboundQueryCompleted {result, .. })) = swarm.select_next_some().await {
+							break result;
 						}
 					};
 
-					if body.len() != 0 {
-						let entry: Entry = serde_json::from_str(&body).unwrap();
-
-						let value = serde_json::to_vec(&entry).unwrap();
-
-						let mut hasher = Sha256::new();
-						hasher.update(format!("{}{}", entry.user, entry.name));
-						let key: String = format!("e_{:X}", hasher.finalize());
-
-						let record = Record {
-							key: Key::new(&key),
-							value,
-							publisher: None,
-							expires: None,
-						};
-
-						println!("{:?}", entry);
-
-						let behaviour = swarm.behaviour_mut();
-						let kad = &mut behaviour.kademlia;
-
-						kad
-							.put_record(record, Quorum::One)
-							.expect("Failed to store record locally.");
-					}
-
-
+					println!("Query response: \n{:?}", res);
 				}
-
 			}
 			line = stdin.select_next_some() => {
 				handle_input(
@@ -139,6 +168,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			}
 		}
 	}
+}
+
+fn get_location_key(input_location: String) -> Key  {
+	let mut key_idx: usize = 0;
+	let parts: Vec<String> = input_location.split("/").map(|s| s.to_string()).collect();
+
+	for (idx, part) in parts.iter().rev().enumerate() {
+		if part.starts_with("e_") {
+			key_idx = parts.len() - idx - 1;
+			break
+		}
+	}
+
+	Key::new(&parts[key_idx])
 }
 
 fn handle_input(behaviour: &mut MyBehaviour, line: String) {
