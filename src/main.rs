489a860f5553ae71d84674f10b84b15ae87f6878
MatsDK <matsdekort257@gmail.com>
2022-04-14 23:49:39 +0100

    format error when put event in dht

--- src/main.rs
+++ src/main.rs
@@ -55,64 +55,98 @@ async fn create_swarm() -> Swarm<MyBehaviour> {
 }
 
 #[derive(Debug)]
-pub struct DhtGetRecord {
+pub struct DhtGetRecordRequest {
 	pub location: String
 }
 
 #[derive(Debug)]
-pub struct DhtPutRecord {
+pub struct DhtPutRecordRequest {
+        pub public_key: String,
 	pub entry: Entry,
 	pub signature: String,
-	pub public_key: String
 }
 
 #[derive(Debug)]
 pub enum DhtRequestType {
-	GetRecord(DhtGetRecord),
-	PutRecord(DhtPutRecord),
+	GetRecord(DhtGetRecordRequest),
+	PutRecord(DhtPutRecordRequest),
+}
+
+#[derive(Debug, Clone)]
+pub struct DhtGetRecordResponse {
+	pub entry: Option<Entry>
+}
+
+impl DhtGetRecordResponse {
+        fn default() -> Result<Response<GetResponse>, Status> {
+                Ok(Response::new(GetResponse {
+                        entry: None
+                }))
+        }
+}
+
+#[derive(Debug, Clone)]
+pub struct DhtPutRecordResponse {
+	pub signature: Option<String>,
+	pub error: Option<String>
+}
+
+#[derive(Debug, Clone)]
+pub enum DhtResponseType {
+	GetRecord(DhtGetRecordResponse),
+	PutRecord(DhtPutRecordResponse),
 }
 
 pub struct MyApi {
 	pub mpsc_sender: mpsc::Sender<DhtRequestType>,
-	pub broadcast_receiver: Arc<Mutex<broadcast::Receiver<Option<Entry>>>>
+	pub broadcast_receiver: Arc<Mutex<broadcast::Receiver<DhtResponseType>>>
 }
 
 #[tonic::async_trait]
 impl Api for MyApi {
 	async fn get(&self, request: Request<GetRequest>) -> Result<Response<GetResponse>, Status> {
-		println!("{}", request.metadata().get("public_key").unwrap().to_str().unwrap());
-		let dht_request = DhtRequestType::GetRecord(DhtGetRecord {
+		let dht_request = DhtRequestType::GetRecord(DhtGetRecordRequest {
 			location: request.get_ref().location.to_owned()
 		});
 
 		self.mpsc_sender.send(dht_request).await;
-
 		match self.broadcast_receiver.lock().await.recv().await {
-			Ok(entry) => {
-				Ok(Response::new(GetResponse {
-					message: format!("get {}", request.get_ref().location),
-					entry
-				}))
-			}
+			Ok(dht_response) => match dht_response {
+                                DhtResponseType::GetRecord(dht_get_response) => {
+                                        Ok(Response::new(GetResponse {
+                                                entry: dht_get_response.entry
+                                        }))
+                                }
+                                _ => DhtGetRecordResponse::default()
+                        }
 			Err(error) => {
 				eprintln!("{}", error);
-				Ok(Response::new(GetResponse {
-					message: format!("get {}", request.get_ref().location),
-					entry: None
-				}))
+                                DhtGetRecordResponse::default()
 			}
 		}
 	}
 
 	async fn put(&self, request: Request<PutRequest>) -> Result<Response<PutResponse>, Status> {
-		let dht_request = DhtRequestType::PutRecord(DhtPutRecord {
+		let dht_request = DhtRequestType::PutRecord(DhtPutRecordRequest {
 			public_key: request.metadata().get("public_key").unwrap().to_str().unwrap().to_string(),
 			signature: request.get_ref().signature.clone(),
 			entry: request.into_inner().entry.unwrap(),
 		});
 
 		self.mpsc_sender.send(dht_request).await;
-		let dht_response = self.broadcast_receiver.lock().await.recv().await.unwrap(); 
+		match self.broadcast_receiver.lock().await.recv().await {
+                        Ok(dht_response) => match dht_response {
+                                DhtResponseType::PutRecord(dht_put_response) => {
+                                        if let Some(error) = dht_put_response.error {
+                                                println!("Error in dht: {}", error);
+                                        }
+                                }
+                                _ => println!("unknown error")
+                        }
+                        Err(error) => {
+				eprintln!("{}", error);
+                        }
+                };
 
 		Ok(Response::new(PutResponse {
 			key: "key".to_string()
@@ -139,7 +173,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let mut dht_swarm = Dht::new(swarm);
 
 	let (mpsc_sender, mpsc_receiver) = mpsc::channel::<DhtRequestType>(32);
-	let (broadcast_sender, broadcast_receiver) = broadcast::channel::<Option<Entry>>(32);
+	let (broadcast_sender, broadcast_receiver) = broadcast::channel::<DhtResponseType>(32);
 
 	tokio::spawn(async move {
 		let mut mpsc_receiver_stream = ReceiverStream::new(mpsc_receiver);
@@ -151,36 +185,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 					match dht_swarm.get(&key).await {
 						Ok(record) => {
-							let record: Entry = serde_json::from_str(&str::from_utf8(&record.value).unwrap()).unwrap();
-							broadcast_sender.send(Some(record)).unwrap();
-						}
+							let entry: Entry = serde_json::from_str(&str::from_utf8(&record.value).unwrap()).unwrap();
+
+							broadcast_sender.send(DhtResponseType::GetRecord(DhtGetRecordResponse { entry: Some(entry) })).unwrap();
+                                                }
 						Err(error) => {
 							eprintln!("{:?}", error);
-							broadcast_sender.send(None).unwrap();
+							broadcast_sender.send(DhtResponseType::GetRecord(DhtGetRecordResponse { entry: None })).unwrap();
 						}
 					};
-				},
+				}
 				DhtRequestType::PutRecord(dht_put_record) => {
-					// let secp = Secp256k1::new();
-					// let secret_key = SecretKey::from_str("566f43d8c8436138c980dcf7f47a960282bd94af418d3f36cac68048a0c8a878").unwrap();
-					// let pub_key = PublicKey::from_secret_key(&secp, &secret_key).to_string();
-
 					let value = serde_json::to_vec(&dht_put_record.entry).unwrap();
 
+					// let secp = Secp256k1::new();
 					// let message = Message::from_hashed_data::<sha256::Hash>(format!("{}/{}", pub_key.to_string(), dht_put_record.entry.name).as_bytes());
 					// let sig = secp.sign_ecdsa(&message, &secret_key);
 
 					let key: String = format!("e_{}", dht_put_record.signature);
 
-					match dht_swarm.put(Key::new(&key.clone()), value).await {
-						Ok(_) => {
-							// broadcast_sender.send(key.to_owned()).unwrap();
-						}
-						Err(error) => {
-							eprintln!("{:?}", error);
-							// broadcast_sender.send(key.to_owned()).unwrap();
-						}
+					let res = match dht_swarm.put(Key::new(&key.clone()), value).await {
+						Ok(_) => DhtResponseType::PutRecord(DhtPutRecordResponse { 
+                                                        signature: Some(key),
+                                                        error: None
+                                                }),
+						Err(error) => DhtResponseType::PutRecord(DhtPutRecordResponse { 
+                                                        signature: None,
+                                                        error: Some(error.to_string())
+                                                })
 					};
+
+                                        broadcast_sender.send(res);
 
 				}
 			};
