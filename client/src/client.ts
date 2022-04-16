05681cd53d794835003fc583f55c00204ef2eb1b
MatsDK <matsdekort257@gmail.com>
2022-04-16 14:15:05 +0100

    create typescript grpc client

---
+++ client/src/client.ts
@@ -0,0 +1,60 @@
+import { loadPackageDefinition, credentials, Metadata } from "@grpc/grpc-js"
+import { loadSync } from "@grpc/proto-loader";
+import path from "path";
+
+const PROTO_PATH = path.join(__dirname, '../proto/api.proto'),
+	SERVER_URL = "192.168.0.164:50051"
+
+const packageDefinition = loadSync(
+	PROTO_PATH,
+	{
+		keepCase: true,
+		longs: String,
+		enums: String,
+		defaults: true,
+		oneofs: true,
+	}
+);
+
+const apiProto = loadPackageDefinition(packageDefinition).api as any;
+
+const client = new apiProto.Api(SERVER_URL, credentials.createInsecure());
+
+const meta = new Metadata();
+meta.add('public_key', '03efb57d7e730b13ca6b23822f4ecbeb7926a529e02befe4e4a8034c56ce607670');
+
+const getEntry = (location: string) => {
+	client.Get(
+		{
+			location
+		},
+		meta,
+		(err: any, data: any) => {
+			console.log(data, err)
+		}
+	)
+}
+
+const putEntry = () => {
+	client.Put(
+		{
+			signature: "304402205cb2bf1b2619f84bf9e88f1b288ad47b982a569d6921d0f62d2548062b6fedb902207043d35fe41b6b272b12379ba04db1b2c68731b36f3f038182e4c6d8aad4850d",
+			entry: {
+				owner: "owner",
+				public: true,
+				read_users: [
+					"User"
+				],
+				name: "Hello"
+			}
+		},
+		meta,
+		(err: any, data: any) => {
+			console.log(data, err)
+		}
+	)
+
+}
+
+putEntry()
+getEntry("e_304402205cb2bf1b2619f84bf9e88f1b288ad47b982a569d6921d0f62d2548062b6fedb902207043d35fe41b6b272b12379ba04db1b2c68731b36f3f038182e4c6d8aad4850d")
