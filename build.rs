322844abf45556195d6d1ac1675351d1079b153b
randymcmillan <randymcmillan@protonmail.com>
2026-07-30 13:18:08 -0400

    intermediate

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
-extern crate tonic_build;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile_protos(&["proto/api.proto"], &["."])?;
+        .compile(&["proto/api.proto"], &["."])?;
     Ok(())
 }
